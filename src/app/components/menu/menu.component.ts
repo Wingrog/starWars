3df6386f069fbcebc1055a5b5bd18514597b84db
Wingrog <ciborgue42@hotmail.fr>
2020-06-23 12:30:42 +0200

    ajout de commentaires

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  date = new Date(); // ajout d'une variable pour afficher la date
+  date = new Date(); // ajout d'une variable pour afficher la date du jour
   name = "Johnny Halliday" //ajout d'une variable qui affiche un nom et prenom
   constructor() { }
 
--- src/app/components/planets/planets.component.ts
+++ src/app/components/planets/planets.component.ts
@@ -11,18 +11,27 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./planets.component.css'],
 })
 export class PlanetsComponent implements OnInit {
-
+  // planets : Il s'agit d'un attribut qui contiendra un tableau de planète. Il n'est pas encore iniatialisé.
+  // Cet attribut est vide (undefined) mais ne pourra contenir qu'un tableau de planète.
   planets: Planet[];
+
+  // Nous injectons ici notre planetService qui nous servira pour récupérer toutes nos planètes.
+  // Nous injectons également le ToastrService pour afficher des notifications aux visiteurs.
   constructor(private planetService: PlanetService, private loggerService: LoggerService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    // Nous initialisons notre attribut planets (this.planets) en lui assignant le tableau de planètes contenu dans           planet.service.ts.
     this.planets = this.planetService.getAllPlanets();
     this.loggerService.loaderService();
   }
 
+  // Fonction appelé lors d'une action de clique (click) sur un lien contenu dans le fichier planets.component.html
+  // Elle prends en paramètre l'objet planete à supprimer.
   deletePlanet(planet: Planet) {
-    this.planets = this.planetService.deletePlanet(planet); //on lance la fonction deletePlanet qui se trouve dans le PlanetService
-    this.toastr.error("La planète " + planet.nom + " à été supprimée!"); //on affiche la notification !
+    // Lors d'un clique on appel la fonction deletePlanet de note planetService avec en paramètre la planète à supprimer.
+    this.planets = this.planetService.deletePlanet(planet);
+    // On affiche la notification à l'utilisateur !
+    this.toastr.error("La planète " + planet.nom + " à été supprimée!");
 
   }
 
