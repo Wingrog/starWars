8ce22aba37e91a6487c7c4c78aeed00d7268fa6c
Wingrog <ciborgue42@hotmail.fr>
2020-06-23 10:45:29 +0200

    generation auto du constructor

--- src/app/models/planet.ts
+++ src/app/models/planet.ts
@@ -1,10 +1,10 @@
 export class Planet {
-  public id: number;
-  public nom: string;
-  public nbHabitant: number;
-  public allegiance: string;
-  public nbKmTerre: number;
-  public image: string;
+  id: number;
+  nom: string;
+  nbHabitant: number;
+  allegiance: string;
+  nbKmTerre: number;
+  image: string;
 
   constructor($id: number = null, $nom: string = null, $nbHabitant: number = null, $allegiance: string = null, $nbKmTerre: number = null, $image: string = null) {
     this.id = $id;
@@ -14,4 +14,5 @@ export class Planet {
     this.nbKmTerre = $nbKmTerre;
     this.image = $image;
   }
+
 };
--- src/app/models/vaisseau.ts
+++ src/app/models/vaisseau.ts
@@ -6,13 +6,12 @@ export class Vaisseau {
   nbPassenger: number;
   image: string;
 
-  constructor(
-    id: number = null, nom: string = null, model: string = null, lightSpeed: boolean = null, nbPassenger: number = null, image: string = null) {
-    this.id = id;
-    this.nom = nom;
-    this.model = model;
-    this.lightSpeed = lightSpeed;
-    this.nbPassenger = nbPassenger;
-    this.image = image;
+  constructor($id: number = null, $nom: string = null, $model: string = null, $lightSpeed: boolean = null, $nbPassenger: number = null, $image: string = null) {
+    this.id = $id;
+    this.nom = $nom;
+    this.model = $model;
+    this.lightSpeed = $lightSpeed;
+    this.nbPassenger = $nbPassenger;
+    this.image = $image;
   }
 }
