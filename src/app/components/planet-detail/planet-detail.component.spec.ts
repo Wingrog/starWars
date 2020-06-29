d3f6f2494a25a4f7abf153c580a17583aebb100b
Wingrog <ciborgue42@hotmail.fr>
2020-06-29 14:52:05 +0200

    correction dun test

--- src/app/components/planet-detail/planet-detail.component.spec.ts
+++ src/app/components/planet-detail/planet-detail.component.spec.ts
@@ -1,5 +1,6 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PlanetDetailComponent } from './planet-detail.component';
 
 describe('PlanetDetailComponent', () => {
@@ -8,9 +9,10 @@ describe('PlanetDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlanetDetailComponent ]
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [PlanetDetailComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
